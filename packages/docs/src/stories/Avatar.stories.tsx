4842619a1e3bdc1d750c131123523328fc0378a6
Claudio Lins <claudio.lins@me.com>
2023-01-22 18:33:29 +0000

    feat: Add core components

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Avatar, AvatarProps } from '@claudioLins-ui/react'
+
+export default {
+  title: 'Data dislay/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/claudio-lins.png',
+    alt: 'Avatar do Claudio Lins',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallBack: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Box, BoxProps, Text } from '@claudioLins-ui/react'
+
+export default {
+  title: 'Surface/Box',
+  component: Box,
+  args: {
+    children: <Text>Testando o elemento Box</Text>,
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
